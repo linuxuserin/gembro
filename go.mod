fc1e59615e755321170a4316216e6553b7728c82
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2021-02-14 12:02:27 +0100

    gtk first

--- go.mod
+++ go.mod
@@ -2,10 +2,4 @@ module gemini
 
 go 1.15
 
-require (
-	github.com/charmbracelet/bubbles v0.7.6
-	github.com/charmbracelet/bubbletea v0.12.4
-	github.com/mattn/go-runewidth v0.0.10
-	github.com/muesli/reflow v0.2.1-0.20210115123740-9e1d0d53df68
-	github.com/muesli/termenv v0.7.4
-)
+require github.com/gotk3/gotk3 v0.5.2
--- main.go
+++ main.go
@@ -1,287 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/url"
-	"os"
 	"strings"
 
-	"github.com/charmbracelet/bubbles/textinput"
-	"github.com/charmbracelet/bubbles/viewport"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/mattn/go-runewidth"
-	"github.com/muesli/reflow/indent"
-	"github.com/muesli/reflow/wordwrap"
-	"github.com/muesli/termenv"
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
 )
-
-type model struct {
-	url        string
-	data       []string
-	textInput  textinput.Model
-	queryInput textinput.Model
-	viewport   viewport.Model
-	cursor     int              // which to-do list item our cursor is pointing at
-	selected   map[int]struct{} // which to-do items are selected
-	err        error
-	links      []LinkPos
-	startURL   string
-	ready      bool
-	history    *History
-	query      string
-}
-
-func (m model) Init() tea.Cmd {
-	if m.startURL != "" {
-		return teaLoadURL(m.startURL, true)
-	}
-	return textinput.Blink
-}
-
-type errMsg error
-
-type TeaResponse struct {
-	*Response
-	AddHist bool
-}
-
-func teaLoadURL(surl string, addHist bool) func() tea.Msg {
-	return func() tea.Msg {
-		u, err := url.Parse(surl)
-		if err != nil {
-			return errMsg(fmt.Errorf("invalid URL: %s", err))
-		}
-		resp, err := loadURL(*u)
-		if err != nil {
-			return errMsg(fmt.Errorf("could not load URL: %s", err))
-		}
-		return TeaResponse{resp, addHist}
-	}
-}
-
-type LinkPos struct {
-	link *Link
-	ypos int
-}
-
-const (
-	headerHeight               = 3
-	footerHeight               = 1
-	useHighPerformanceRenderer = false
-	nindent                    = 4
-)
-
-func parseContent(content []string, cursor int) (string, []LinkPos) {
-	var linkIDX int
-	var s strings.Builder
-	var links []LinkPos
-	var ypos int
-	p := termenv.ColorProfile()
-	s.WriteString("\n\n")
-	for _, line := range content {
-		if strings.HasPrefix(line, "=>") {
-			l, err := ParseLink(line)
-			if err != nil {
-				l = &Link{URL: "", Name: "Invalid link: " + line}
-			}
-			links = append(links, LinkPos{l, ypos})
-
-			linkIDX++
-			oline := termenv.String(fmt.Sprintf("> %s (%s)", l.Name, l.URL)).Foreground(p.Color("#0000ff"))
-			if linkIDX == cursor {
-				oline = oline.Reverse()
-			}
-			s.WriteString(oline.String())
-			s.WriteString("\n")
-			ypos++
-			continue
-		}
-		sl := wordwrap.String(line, 100)
-		s.WriteString(sl)
-		s.WriteString("\n")
-		ypos += strings.Count(sl, "\n") + 1
-	}
-	return indent.String(s.String(), nindent), links
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmds []tea.Cmd
-
-	updateContent := func() {
-		if m.cursor == 0 {
-			m.textInput.Focus()
-		} else {
-			m.textInput.Blur()
-		}
-		content, links := parseContent(m.data, m.cursor)
-		m.links = links
-		m.viewport.SetContent(content)
-		if m.cursor != 0 {
-			ypos := links[m.cursor-1].ypos
-			if m.viewport.YOffset > ypos {
-				m.viewport.YOffset = ypos
-			}
-			if m.viewport.YOffset+m.viewport.Height <= ypos {
-				m.viewport.YOffset = ypos - m.viewport.Height + 1
-			}
-		}
-	}
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyShiftTab:
-			if m.cursor > 0 {
-				m.cursor--
-				updateContent()
-			}
-			return m, nil
-		case tea.KeyTab:
-			m.cursor = (m.cursor + 1) % (len(m.links) + 1)
-			updateContent()
-			return m, nil
-		case tea.KeyCtrlC:
-			return m, tea.Quit
-		case tea.KeyEsc:
-			if m.textInput.Focused() {
-				m.textInput.Blur()
-			} else {
-				m.textInput.Focus()
-			}
-			return m, nil
-		case tea.KeyEnter:
-			m.err = nil
-			if m.query != "" && m.queryInput.Value() != "" {
-				return m, teaLoadURL(m.textInput.Value()+"?"+m.queryInput.Value(), true)
-			}
-			if m.cursor == 0 {
-				return m, teaLoadURL(m.textInput.Value(), true)
-			}
-			surl := m.links[m.cursor-1].link.FullURL(m.url)
-			return m, teaLoadURL(surl, true)
-		case tea.KeyLeft:
-			if prev, ok := m.history.Back(); ok {
-				return m, teaLoadURL(prev, false)
-			}
-			return m, nil
-		case tea.KeyRight:
-			if next, ok := m.history.Forward(); ok {
-				return m, teaLoadURL(next, false)
-			}
-			return m, nil
-		}
-
-	case tea.WindowSizeMsg:
-		verticalMargins := headerHeight + footerHeight
-
-		if !m.ready {
-			// Since this program is using the full size of the viewport we need
-			// to wait until we've received the window dimensions before we
-			// can initialize the viewport. The initial dimensions come in
-			// quickly, though asynchronously, which is why we wait for them
-			// here.
-			m.viewport = viewport.Model{Width: msg.Width, Height: msg.Height - verticalMargins}
-			m.viewport.YPosition = headerHeight
-			m.viewport.HighPerformanceRendering = useHighPerformanceRenderer
-			m.ready = true
-		} else {
-			m.viewport.Width = msg.Width
-			m.viewport.Height = msg.Height - verticalMargins
-		}
-
-		if useHighPerformanceRenderer {
-			// Render (or re-render) the whole viewport. Necessary both to
-			// initialize the viewport and when the window is resized.
-			//
-			// This is needed for high-performance rendering only.
-			cmds = append(cmds, viewport.Sync(m.viewport))
-		}
-
-	case errMsg:
-		m.err = msg
-		m.data = nil
-		m.links = nil
-		m.cursor = 0
-		m.viewport.SetContent(msg.Error())
-		return m, nil
-	case TeaResponse:
-		m.cursor = 0
-		m.data = strings.Split(msg.Body, "\n")
-		content, links := parseContent(m.data, m.cursor)
-		m.links = links
-		m.viewport.SetContent(content)
-		m.url = msg.URL
-		m.textInput.SetValue(msg.URL)
-		if msg.AddHist {
-			m.history.Add(msg.URL)
-		}
-		if msg.Header.Status/10 == 1 {
-			m.query = msg.Header.Meta
-			m.queryInput.Focus()
-		} else {
-			m.query = ""
-		}
-		return m, nil
-	}
-
-	var cmd tea.Cmd
-	m.viewport, cmd = m.viewport.Update(msg)
-	if useHighPerformanceRenderer {
-		cmds = append(cmds, cmd)
-	}
-	m.textInput, cmd = m.textInput.Update(msg)
-	cmds = append(cmds, cmd)
-	if m.query != "" {
-		m.queryInput, cmd = m.queryInput.Update(msg)
-		cmds = append(cmds, cmd)
-	}
-	return m, tea.Batch(cmds...)
-}
-
-func (m model) View() string {
-	if !m.ready {
-		return "\n  Initalizing..."
-	}
-
-	headerMid := "│ Gemini ├"
-	headerMid += strings.Repeat("─", m.viewport.Width-runewidth.StringWidth(headerMid))
-
-	footerMid := fmt.Sprintf("┤ %3.f%% │", m.viewport.ScrollPercent()*100)
-	gapSize := m.viewport.Width - runewidth.StringWidth(footerMid)
-	footerMid = strings.Repeat("─", gapSize) + footerMid
-
-	inputs := m.textInput.View()
-	if m.query != "" {
-		inputs += fmt.Sprintf(" %s: %s", m.query, m.queryInput.View())
-	}
-
-	return fmt.Sprintf("%s\n\n%s\n%s\n%s", inputs, headerMid, m.viewport.View(), footerMid)
-}
-
-func initialModel(surl string) model {
-	i := textinput.NewModel()
-	i.Width = 200
-	i.SetValue(surl)
-	i.Focus()
-
-	qi := textinput.NewModel()
-	qi.Width = 200
-
-	return model{
-		data:       nil,
-		textInput:  i,
-		queryInput: qi,
-		startURL:   surl,
-		history:    &History{},
-
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
-	}
-}
 
 func debugURL(surl string) error {
 	u, err := url.Parse(surl)
@@ -297,6 +27,79 @@ func debugURL(surl string) error {
 	return nil
 }
 
+type App struct {
+	Content *gtk.Box
+	Spin    *gtk.Spinner
+}
+
+func (a *App) uiLoadURL(surl string) {
+	a.Spin.Start()
+	go func() {
+		u, err := url.Parse(surl)
+		if err != nil {
+			log.Printf("invalid url: %s", err)
+			return
+		}
+		resp, err := loadURL(*u)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		tmpl := template.Must(template.New("foo").Parse(`<a href="{{.URL}}" title="{{.URL}}">{{.Name}}</a>`))
+		renderLink := func(l *Link) string {
+			var b bytes.Buffer
+			if err := tmpl.Execute(&b, struct {
+				URL  template.URL
+				Name string
+			}{template.URL(l.FullURL(resp.URL)), l.Name}); err != nil {
+				log.Print(err)
+				return "Render failure"
+			}
+			return b.String()
+		}
+
+		lines := strings.Split(resp.Body, "\n")
+		for i, line := range lines {
+			if strings.HasPrefix(line, "=>") {
+				l, err := ParseLink(line)
+				if err != nil {
+					l = &Link{"", "Invalid link: " + line}
+				}
+				rl := renderLink(l)
+				lines[i] = rl
+			}
+		}
+
+		l, err := gtk.LabelNew("")
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		l.SetMarkup(strings.Join(lines, "\n"))
+		l.SetSelectable(true)
+		l.SetLineWrap(true)
+		l.SetHAlign(gtk.ALIGN_START)
+		_, _ = l.Connect("activate-link", func(l *gtk.Label, url string) bool {
+			if strings.HasPrefix(url, "gemini://") {
+				a.uiLoadURL(url)
+				return true
+			}
+			return false
+		})
+
+		_, _ = glib.IdleAdd(func() {
+			w := a.Content
+			w.GetChildren().Foreach(func(i interface{}) {
+				w.Remove(i.(gtk.IWidget))
+			})
+			w.Add(l)
+			w.ShowAll()
+			a.Spin.Stop()
+		})
+	}()
+}
+
 func run() error {
 	surl := flag.String("url", "", "URL to start with")
 	durl := flag.String("debug", "", "Debug URL")
@@ -306,15 +109,90 @@ func run() error {
 		return debugURL(*durl)
 	}
 
-	p := tea.NewProgram(initialModel(*surl))
-	p.EnterAltScreen()
-	defer p.ExitAltScreen()
-	// p.EnableMouseCellMotion()
-	// defer p.DisableMouseCellMotion()
-	if err := p.Start(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
+	app := App{}
+
+	gtk.Init(nil)
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
 	}
+	win.SetTitle("Simple Example")
+	_, _ = win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	b, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 10)
+	if err != nil {
+		return err
+	}
+	win.Add(b)
+
+	hb, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 10)
+	if err != nil {
+		return err
+	}
+	b.Add(hb)
+
+	e, err := gtk.EntryNew()
+	if err != nil {
+		return err
+	}
+	e.SetHExpand(true)
+	e.SetText(*surl)
+	hb.Add(e)
+
+	f, err := gtk.ScrolledWindowNew(nil, nil)
+	if err != nil {
+		return err
+	}
+	f.SetVExpand(true)
+
+	cb, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	if err != nil {
+		return err
+	}
+	app.Content = cb
+	cb.SetMarginStart(10)
+	cb.SetMarginEnd(10)
+	f.Add(cb)
+
+	l, err := gtk.ButtonNew()
+	if err != nil {
+		return err
+	}
+	l.SetLabel("Go")
+	_, _ = l.Connect("clicked", func() {
+		s, _ := e.GetText()
+		app.uiLoadURL(s)
+	})
+	hb.Add(l)
+
+	spin, err := gtk.SpinnerNew()
+	if err != nil {
+		return err
+	}
+	app.Spin = spin
+	hb.Add(spin)
+
+	b.Add(f)
+
+	// Set the default window size.
+	win.SetDefaultSize(800, 600)
+	win.SetPosition(gtk.WIN_POS_CENTER)
+
+	// Recursively show all widgets contained in this window.
+	win.ShowAll()
+
+	if *surl != "" {
+		go func() {
+			_, _ = glib.IdleAdd(func() {
+				app.uiLoadURL(*surl)
+			})
+		}()
+	}
+
+	gtk.Main()
+
 	return nil
 }
 
