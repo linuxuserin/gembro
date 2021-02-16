e16d860bd8eccf3b794be77c6bf626668bf289b4
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2021-02-16 14:09:26 +0100

    GTK just sucks, it was actually crashing X repeatedly while debugging. I guess doing GUIs with Go (or in general) is just kind of a pain. I'm now doing it the old fashioned way. Serve if over html.

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module git.sr.ht/~rafael/gemini-browser
 
 go 1.15
 
-require (
-	github.com/gotk3/gotk3 v0.5.2
-	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
-)
+require golang.org/x/net v0.0.0-20210119194325-5f4716e94777
--- main.go
+++ main.go
@@ -2,25 +2,15 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
-	"html"
 	"html/template"
 	"log"
+	"net/http"
 	"net/url"
 	"strings"
 
-	"git.sr.ht/~rafael/gemini-browser/internal/bookmark"
 	"git.sr.ht/~rafael/gemini-browser/internal/gemini"
-	"git.sr.ht/~rafael/gemini-browser/internal/history"
-	"github.com/gotk3/gotk3/glib"
-	"github.com/gotk3/gotk3/gtk"
-)
-
-const (
-	appTitle = "Gemini Browser"
-	homeURL  = "home://"
 )
 
 func debugURL(surl string) error {
@@ -37,308 +27,132 @@ func debugURL(surl string) error {
 	return nil
 }
 
-type LinkOffset struct {
-	Start, End int
-	URL        string
-}
-type App struct {
-	Spin           *gtk.Spinner
-	content        *gtk.TextBuffer
-	textView       *gtk.TextView
-	Entry          *gtk.Entry
-	bookmarkButton *gtk.Button
-	History        history.History
-	Bookmarks      *bookmark.BookmarkStore
-	currentURL     string
-	tags           map[string]*gtk.TextTag
-	links          []LinkOffset
-	cancelFunc     context.CancelFunc
-}
-
-func (a *App) setBookmarkIcon(has bool) {
-	if has {
-		a.bookmarkButton.SetLabel("★")
-	} else {
-		a.bookmarkButton.SetLabel("☆")
+func format(html string, args ...string) string {
+	nargs := make([]interface{}, len(args))
+	for i, arg := range args {
+		nargs[i] = template.HTMLEscapeString(arg)
 	}
+	return fmt.Sprintf(html, nargs...)
 }
 
-func (a *App) promptQuery(title, surl string) {
-	_, _ = glib.IdleAdd(func() {
-		d, err := gtk.DialogNewWithButtons(title, nil, 0)
-		if err != nil {
-			log.Print(err)
-			return
-		}
-		inp, err := gtk.EntryNew()
-		if err != nil {
-			log.Print(err)
-			return
-		}
-		d.AddActionWidget(inp, gtk.RESPONSE_ACCEPT)
-		if _, err := d.AddButton("OK", gtk.RESPONSE_ACCEPT); err != nil {
-			log.Print(err)
-			return
-		}
+var style = `
+	body {background: black; color: white; font-family: monospace}
+	.h1 {color: red; font-weight: bold}
+	.h2 {color: yellow; font-weight: bold}
+	a {color: #58a6ff}
+	pre {margin:0}
+	.outer {margin: 0 auto; max-width: 600px; padding-top: 20px; overflow-wrap: anywhere}
+`
 
-		_, _ = d.Connect("response", func(_ *gtk.Dialog, r gtk.ResponseType) {
-			defer d.Destroy()
-			switch r {
-			case gtk.RESPONSE_ACCEPT:
-				s, err := inp.GetText()
-				if err != nil {
-					log.Print(err)
-					return
-				}
-				s = strings.TrimSpace(s)
-				if s != "" {
-					a.gotoURL(fmt.Sprintf("%s?%s", surl, url.QueryEscape(s)), true)
-				}
+func geminiToHTML(input, parentURL string) string {
+	lines := strings.Split(input, "\n")
+	pageTitle := ""
+	var mono bool
+	for i, line := range lines {
+		if strings.HasPrefix(line, "# ") {
+			lines[i] = format(`<span class="h1">%s</span><br>`, line)
+			if pageTitle == "" {
+				pageTitle = line[2:]
 			}
-		})
-		d.ShowAll()
-	})
-}
-
-func (a *App) homeMeta() string {
-	var s strings.Builder
-	s.WriteString("# Bookmarks\n\n")
-	for _, b := range a.Bookmarks.All() {
-		s.WriteString(fmt.Sprintf("=> %s %s\n", b.URL, b.Name))
-	}
-	return s.String()
-}
-
-var linkTmpl = template.Must(template.New("foo").Parse(`<a href="{{.URL}}" title="{{.URL}}">{{.Name}}</a> ` +
-	`{{if .Type}}({{.Type}}){{end}}`))
-
-func renderLink(surl, name string) string {
-	surl = html.EscapeString(surl)
-	var typ string
-	if !strings.HasPrefix(surl, "gemini://") {
-		typ = strings.Split(surl, "://")[0]
-	}
-	var b strings.Builder
-	if err := linkTmpl.Execute(&b, struct {
-		URL        template.URL
-		Name, Type string
-	}{template.URL(surl), name, typ}); err != nil {
-		log.Print(err)
-		return "Render failure"
-	}
-	return b.String()
-}
-
-func (a *App) renderMeta(meta string, surl string) {
-	_, _ = glib.IdleAdd(func() {
-		a.Entry.SetText(surl)
-		if surl != "" {
-			a.setBookmarkIcon(a.Bookmarks.Contains(a.currentURL))
-		} else {
-			a.setBookmarkIcon(false)
+			continue
 		}
-
-		a.content.SetText("")
-
-		a.links = nil
-		var p []string
-		var mono bool
-		iter := a.content.GetStartIter()
-		addP := func() {
-			if len(p) == 0 {
-				return
-			}
-			joined := strings.Join(p, "\n")
+		if strings.HasPrefix(line, "## ") {
+			lines[i] = format(`<span class="h2">%s</span><br>`, line)
+			continue
+		}
+		if strings.HasPrefix(line, "### ") {
+			lines[i] = format(`<span class="h3">%s</span><br>`, line)
+			continue
+		}
+		if strings.HasPrefix(line, "```") {
+			mono = !mono
 			if mono {
-				a.content.InsertWithTag(iter, joined+"\n", a.tags["mono"])
+				lines[i] = "<pre>"
 			} else {
-				a.content.Insert(iter, joined+"\n")
+				lines[i] = "</pre>"
 			}
-			p = nil
+			continue
 		}
-
-		for _, line := range strings.Split(meta, "\n") {
-			if strings.HasPrefix(line, "# ") {
-				addP()
-				a.content.InsertWithTag(iter, line[2:]+"\n", a.tags["h1"])
-				continue
-			}
-			if strings.HasPrefix(line, "## ") {
-				addP()
-				a.content.InsertWithTag(iter, line[3:]+"\n", a.tags["h2"])
-				continue
-			}
-			if strings.HasPrefix(line, "### ") {
-				addP()
-				a.content.InsertWithTag(iter, line[4:]+"\n", a.tags["h3"])
-				continue
-			}
-			if strings.HasPrefix(line, "=>") {
-				addP()
-				link, err := gemini.ParseLink(line)
-				if err != nil {
-					link = &gemini.Link{URL: "", Name: "Invalid link"}
-				}
-				furl := link.FullURL(surl)
-				_ = furl
-
-				startOffset := iter.GetOffset()
-				a.content.InsertWithTag(iter, link.Name, a.tags["link"])
-				if !strings.HasPrefix(furl, "gemini://") {
-					a.content.Insert(iter, fmt.Sprintf(" (%s)", strings.Split(furl, "://")[0]))
-				}
-				a.links = append(a.links, LinkOffset{startOffset, iter.GetOffset(), furl})
-				a.content.Insert(iter, "\n")
-				continue
-			}
-			if strings.HasPrefix(line, "```") {
-				addP()
-				mono = !mono
-				continue
-			}
-			p = append(p, line)
-		}
-		addP()
-
-		a.textView.ShowAll()
-		a.Spin.Stop()
-	})
-}
-
-func (a *App) spin(start bool) {
-	_, _ = glib.IdleAdd(func() {
-		if start {
-			a.Spin.Start()
-			return
-		}
-		a.Spin.Stop()
-	})
-}
-
-func (a *App) loadURL(surl string) (*gemini.Response, error) {
-	switch surl {
-	case homeURL:
-		var r gemini.Response
-		r.Body = a.homeMeta()
-		r.Header.Status = 2
-		r.URL = surl
-		return &r, nil
-	default:
-		u, err := url.Parse(surl)
-		if err != nil {
-			return nil, fmt.Errorf("invalid url: %s", err)
-		}
-		a.cancelFunc()
-		ctx, done := context.WithCancel(context.Background())
-		a.cancelFunc = done
-		return gemini.LoadURL(ctx, *u)
-	}
-}
-
-func (a *App) gotoURL(surl string, addHistory bool) {
-	a.gotoURLDepth(surl, addHistory, 0)
-}
-
-func (a *App) gotoURLDepth(surl string, addHistory bool, depth int) {
-	a.spin(true)
-	go func() {
-		stopSpinning := true
-		defer func() {
-			if stopSpinning {
-				a.spin(false)
-			}
-		}()
-		var body string
-		resp, err := a.loadURL(surl)
-		if err != nil {
-			log.Print(err)
-			if errors.Is(err, context.Canceled) {
-				body = "# Page canceled\n\nYou canceled this page"
-			} else {
-				body = "# Could not render the page\n\nThe server did not respond with something worthy"
-			}
-			resp = &gemini.Response{Body: body, Header: gemini.Header{Status: 2}, URL: surl}
-		}
-		switch resp.Header.Status {
-		case 1:
-			a.promptQuery(resp.Header.Meta, surl)
-			return
-		case 2:
-			body, err = resp.GetBody()
+		if strings.HasPrefix(line, "=>") {
+			link, err := gemini.ParseLink(line)
 			if err != nil {
-				log.Print(err)
-				body = "# Error rendering content\n\nSorry :-("
+				link = &gemini.Link{URL: "", Name: "Invalid link"}
 			}
-		case 3: // Redirect
-			if depth < 5 {
-				stopSpinning = false
-
-				// Get URL to redirect to
-				// It might be relative so use current url
-				u, err := url.Parse(surl)
-				if err != nil {
-					return
-				}
-				u, err = u.Parse(resp.Header.Meta)
-				if err != nil {
-					return
-				}
-
-				if u.Scheme == "gemini" {
-					a.gotoURLDepth(u.String(), addHistory, depth+1)
-					return
-				}
-
-				body = fmt.Sprintf(
-					`# Wrong scheme`+
-						"\n\nThe page tried to redirect to a non-gemini URL.\n\nGo there anyway:\n=> %s",
-					resp.Header.Meta)
+			furl := link.FullURL(parentURL)
+			if strings.HasPrefix(furl, "gemini://") {
+				furl = fmt.Sprintf("?url=%s", furl)
+				lines[i] = format(`<a href="%[1]s" title="%[1]s">%[2]s</a><br>`,
+					furl, link.Name)
 			} else {
-				body = fmt.Sprintf(
-					`# 👹 Welcome to the Web From Hell 👹`+
-						"\n\nThe page redirected more than 5 times.\n\nRedirect (up to) 5 more times:\n=> %s",
-					resp.Header.Meta)
+				lines[i] = format(`<a href="%[1]s" title="%[1]s" target="_blank">%[2]s</a><br>`, furl, link.Name)
 			}
-		case 4:
-			body = fmt.Sprintf("# Temporary failure \n\nMessage: %s", resp.Header.Meta)
-		case 5:
-			if resp.Header.StatusDetail == 1 {
-				body = "# Page not found\n\nThis page does not exist 🤷"
-			} else {
-				body = fmt.Sprintf("# Permanent failure \n\nMessage: %s", resp.Header.Meta)
-			}
-		case 6:
-			body = fmt.Sprintf("# Client certificate required \n\nBecause: %s", resp.Header.Meta)
-		default:
-			body = "# Unexpected response\n\nNot much that I can do with this, human"
+			continue
 		}
-		if addHistory {
-			a.History.Add(resp.URL)
-		}
-		a.currentURL = resp.URL
-
-		a.renderMeta(body, a.currentURL)
-	}()
+		lines[i] = format(`<span>%s</span><br>`, line)
+	}
+	if mono {
+		lines = append(lines, "</pre>")
+	}
+	if pageTitle == "" {
+		pageTitle = parentURL
+	}
+	return fmt.Sprintf(`<!doctype html><html><head><title>%s</title><style>%s</style></head>`+
+		`<body><div class="outer">%s</div></body></html>`,
+		pageTitle, style, strings.Join(lines, ""))
 }
 
 func run() error {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	surl := flag.String("url", "", "URL to start with")
 	durl := flag.String("debug", "", "Debug URL")
+	host := flag.String("host", "localhost:8080", "Debug URL")
 	flag.Parse()
 
 	if *durl != "" {
 		return debugURL(*durl)
 	}
 
-	bs, err := bookmark.Load("bookmarks.json")
-	if err != nil {
-		return err
-	}
+	// app := App{Bookmarks: bs, cancelFunc: func() {}}
+	return http.ListenAndServe(*host, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rurl := r.FormValue("url")
+		if rurl != "" {
+			u, err := url.Parse(rurl)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			resp, err := gemini.LoadURL(r.Context(), *u)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			switch resp.Header.Status {
+			case 1:
+			case 2:
+				b, err := resp.GetBody()
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+					return
+				}
+				if r.FormValue("src") == "1" {
+					fmt.Fprint(w, b)
+					return
+				}
+				w.Header().Set("Content-type", "text/html; charset=utf-8")
+				fmt.Fprint(w, geminiToHTML(b, rurl))
+			case 3:
+				u, err := u.Parse(resp.Header.Meta)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+					return
+				}
+				http.Redirect(w, r, fmt.Sprintf("?url=%s", u), http.StatusMovedPermanently)
+			}
+			return
+		}
 
-	app := App{Bookmarks: bs, cancelFunc: func() {}}
-	return app.loadMainUI(*surl)
+		http.Error(w, "Nothing to do", http.StatusBadRequest)
+	}))
 }
 
 func main() {
