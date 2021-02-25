dfef102899fe23ab21533d419340ee1933473bc3
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2021-02-25 15:51:43 +0100

    Add margin

--- gemtext.go
+++ gemtext.go
@@ -17,6 +17,8 @@ const (
 	palegoldenrod  = "#EEE8AA" // code
 )
 
+const textWidth = 80
+
 var colors = termenv.ColorProfile()
 
 func color(input, color string) string {
@@ -28,26 +30,34 @@ type linkPos struct {
 	url, name string
 }
 
-func parseContent(data string, width int, baseURL neturl.URL) (content string, links []linkPos, title string) {
+func parseContent(data string, availableWidth int, baseURL neturl.URL) (
+	content string, links []linkPos, title string) {
+
 	var s strings.Builder
 	var mono bool
-	var ypos int
+	margin := (availableWidth - textWidth) / 2
+	indent := strings.Repeat(" ", margin)
+	fmt.Fprintln(&s)
+	ypos := 1
 	for _, line := range strings.Split(data, "\n") {
 		if strings.HasPrefix(line, "```") {
 			mono = !mono
 			continue
 		}
 		if !mono && strings.HasPrefix(line, "# ") {
+			fmt.Fprint(&s, indent)
 			fmt.Fprintln(&s, color(line[2:], red))
 			ypos++
 			continue
 		}
 		if !mono && strings.HasPrefix(line, "## ") {
+			fmt.Fprint(&s, indent)
 			fmt.Fprintln(&s, color(line[3:], yellow))
 			ypos++
 			continue
 		}
 		if !mono && strings.HasPrefix(line, "### ") {
+			fmt.Fprint(&s, indent)
 			fmt.Fprintln(&s, color(line[4:], fuchsia))
 			ypos++
 			continue
@@ -63,20 +73,25 @@ func parseContent(data string, width int, baseURL neturl.URL) (content string, l
 			if furl.Scheme != "gemini" {
 				extra = fmt.Sprintf(" (%s)", furl.Scheme)
 			}
+			fmt.Fprint(&s, indent)
 			fmt.Fprintf(&s, "> %s%s\n", color(l.Name, cornflowerblue), extra)
 			links = append(links, linkPos{y: ypos, url: furl.String(), name: l.Name})
 			ypos++
 			continue
 		}
 		if mono {
+			fmt.Fprint(&s, indent)
 			fmt.Fprintln(&s, color(line, palegoldenrod))
 			ypos++
 			continue
 		}
 
-		sl := LineWrap(line, width)
-		fmt.Fprintln(&s, sl)
-		ypos += strings.Count(sl, "\n") + 1
+		sl := LineWrap(line, textWidth)
+		for _, line := range strings.Split(sl, "\n") {
+			fmt.Fprint(&s, indent)
+			fmt.Fprintln(&s, line)
+			ypos++
+		}
 	}
 	if title == "" {
 		title = baseURL.String()
